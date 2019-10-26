a1d3b9930091a02629c12d4c11c5230da0dc55c7
Ken Swan <Thekenswan@gmail.com>
2019-10-26 18:33:19 -0500

    bodyparser and method override installed

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express') 
 const app = express()
 const PORT = 3000
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override'); 
 
 
 
 require('./db/db');
 
 
+app.use(methodOverride('_method')); 
+app.use(bodyParser.urlencoded({extended: false})); 
 
+const usersController = require('./controllers/users.js');
+app.use('/auth', usersController) 
 
 
 
