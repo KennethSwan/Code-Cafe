b539b8b31f5a23116e1759ebb459ca3ba51e6732
Kristiana Blake <kristiana.blake@gmail.com>
2019-10-28 16:16:23 -0500

    added express to places controller

--- controllers/places.js
+++ controllers/places.js
@@ -1,1 +1,12 @@
-places.js
+const express = require('express');
+const router = express.Router();
+const User = require('../models/places');
+const bcrypt = require('bcrypt')
+
+router.get('', async(req, res, next) => {
+	try{
+
+	} else {
+		
+	}
+})
