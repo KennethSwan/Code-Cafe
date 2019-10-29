0926d9ec802841a43a4b103afaf21ad4ce82699b
Ken Swan <Thekenswan@gmail.com>
2019-10-29 12:49:53 -0500

    zip code search shows list of 20 shops in that zip code

--- controllers/places.js
+++ controllers/places.js
@@ -9,13 +9,19 @@ router.get('/', (req, res) => {
 })
 
 router.post('/search', async(req, res, next) => {
-	const zip = req.body.zip
+	console.log("\n here's req.body (req.body) in POST /places/search");
+	console.log(req.body);
+	const zip = req.body.zipcode
+	console.log("\n here's zip (req.body.zip) in POST /places/search");
+	console.log(zip);
 	const url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=coffeeshops+in+"+zip+"&key=AIzaSyBHFRhxiNyFGr1xAOPOwOdtTY9PI3HEdDE";
+	console.log("\n here's URL we're going to search in POST /places/search");
+	console.log(url);
 	try{
 		const dataFromGoogle = await superAgent.get(url)
 		console.log("\n here's the data back from google");
 		console.log(dataFromGoogle.body.results);
-		res.render('places/index.ejs', {
+		res.render('./places/index.ejs', {
 			dataFromGoogle: dataFromGoogle.body.results
 		})
 	} catch (err) {
--- server.js
+++ server.js
@@ -22,10 +22,14 @@ const secretInfo = require('./secretInfo.js')
 const usersController = require('./controllers/users.js');
 app.use('/users', usersController) 
 
+// everything that should be handled by places controller
 const placesController = require('./controllers/places.js'); 
+// so urls should start with /places
 app.use('/places', placesController)
 
+// everything that should be handled by reviews controller
 const reviewsController = require('./controllers/reviews.js'); 
+// so urls should start with /reviews
 app.use('/reviews', reviewsController)
 
 
