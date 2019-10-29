92e1150b6852dbf5ef6ac4b81eea0702b24e8499
Ken Swan <Thekenswan@gmail.com>
2019-10-29 17:18:50 -0500

    User can now search by zip, click link for coffee shop and be showing information on show page for that shop

--- controllers/places.js
+++ controllers/places.js
@@ -5,42 +5,63 @@ const superAgent = require('superagent')
 
 
 router.get('/', (req, res) => {
-	res.render('./places/search.ejs')
+	res.render('places/search.ejs')
 })
 
 router.post('/search', async(req, res, next) => {
-	console.log("\n here's req.body (req.body) in POST /places/search");
-	console.log(req.body);
 	const zip = req.body.zipcode
-	console.log("\n here's zip (req.body.zip) in POST /places/search");
-	console.log(zip);
 	const url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=coffeeshops+in+"+zip+"&key=AIzaSyBHFRhxiNyFGr1xAOPOwOdtTY9PI3HEdDE";
-	console.log("\n here's URL we're going to search in POST /places/search");
-	console.log(url);
 	try{
 		const dataFromGoogle = await superAgent.get(url)
-		console.log("\n here's the data back from google");
-		console.log(dataFromGoogle.body.results);
-		res.render('./places/index.ejs', {
+		console.log("\n here's data from google in POST /search dataFromGoogle", dataFromGoogle.body.results);
+		res.render('places/index.ejs', {
 			dataFromGoogle: dataFromGoogle.body.results
 		})
 	} catch (err) {
 		next(err);
 	}
-
 }); 
 
-router.get('/show', async(req, res, next) => {
-	try {
-		const allCoffee = await dataFromGoogle.body.results
-		res.render('places/show.ejs', {
-			data: allCoffee
-		});
+// router.get('/search', (req, res) => {
+// // 	res.render('/places/show.ejs')
+// });
 
-	} catch(err) {
+// router.get('/show', async(req, res, next) => {
+// 	try {
+// 		const allCoffee = await dataFromGoogle.body.results
+// 		res.render('places/show.ejs', {
+// 			data: allCoffee
+// 		});
+// 	} catch(err) {
+// 		next(err)
+// 	}
+// });
+
+router.get('/search/:place_id', async (req, res, next) => {
+
+	// get data from google Place Details -- info about one place
+	const placeId = req.params.place_id
+
+	// use req.params.place_id --  build a URL
+	const url = "https://maps.googleapis.com/maps/api/place/details/json?place_id="+placeId+"&key=AIzaSyBHFRhxiNyFGr1xAOPOwOdtTY9PI3HEdDE";
+	// log data make sure it's good
+	console.log("\n here's the url in place details search");
+	console.log(url);
+	console.log(req.params.place_id);
+
+
+	try{
+		const dataFromGoogle = await superAgent.get(url)
+		console.log("\n here's the result from google ", );
+		res.render('places/show.ejs', {
+			dataFromGoogle: dataFromGoogle.body.result
+		})
+		// res.render('places/show.ejs', { dataFromGoogle: cafe })
+	} catch(err){
 		next(err)
 	}
-});
+
+})
 
 
 
