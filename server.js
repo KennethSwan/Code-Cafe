b191b482221bfe96bf03cd8173be81fe60e416c1
Ken Swan <Thekenswan@gmail.com>
2019-10-31 12:09:16 -0500

    Updated code for heroku deployment

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+// console.log(process.env);
 const express = require('express') 
 const app = express()
 const PORT = 3000
@@ -9,15 +11,13 @@ const session = require('express-session')
 require('./db/db');
 
 app.use(session({
-	secret: "this is a random secret string",
+	secret: process.env.SESSION_SECRET,
 	resave: false,
 	saveUninitialized: false
 }));
 
 app.use(methodOverride('_method')); 
 app.use(bodyParser.urlencoded({extended: false})); 
-
-const secretInfo = require('./secretInfo.js')
 
 const usersController = require('./controllers/users.js');
 app.use('/users', usersController) 
