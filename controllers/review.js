bf69967a2d356360eeef0084d95e81ecc2b5bb3d
Ken Swan <Thekenswan@gmail.com>
2019-11-03 23:52:56 -0600

    finished debugging to get nodemon working

--- controllers/review.js
+++ controllers/review.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const Review = require("../models/review")
-const User = require("..models/user")
+const Review = require('../models/review');
+const User = require('../models/user');
 const superAgent = require('superagent')
 
 
