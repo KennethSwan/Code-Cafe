90ec7eefbe9ee281e6e86ea10d9946d67c0bf05d
Kristiana Blake <kristiana.blake@gmail.com>
2019-11-04 23:51:36 -0600

    deleted extra review module

--- models/cafeList.js
+++ models/cafeList.js
@@ -1,7 +1,34 @@
-cafeList = [
+const cafeList = [
 	{ 
-		name: 
-		address:
-		text
+		name: "Goddess and the Baker",
+		address: "33 W Wabash Avenue"
+		review: String, 
+	},
+	{
+		name: "Dollop Coffee",
+		address: "801 S Financial Place"
+		review: String, 
+	},
+	{
+		name: "The Wormhole Coffee",
+		address: "1462 N Milwaukee Avenue"
+		review: String, 
+	},
+	{
+		name: "Swada Coffee",
+		address: "112 N Green Street"
+		review: String, 
+	},
+	{
+		name: "Cafe Integral at Freehand Chicago",
+		address: "19 E Ohio St"
+		review: String, 
+	},
+	{
+		name: "Stumptown Coffee Roasters",
+		address: "311 N Morgan St"
+		review: String, 
 	}
 ];
+
+module.exports = cafeList; 
--- models/user.js
+++ models/user.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose'); 
 
 const userSchema = new mongoose.Schema({
-	username: String,
-	password: String
+	username: {
+		type: String,
+		requireed: true
+	},
+	password:{
+		type: String, 
+		required: true
+	} 
+	//review 
 })
 
 const User = mongoose.model('User', userSchema);
