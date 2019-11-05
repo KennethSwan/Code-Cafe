420e87a3ce6a48ac1a10ce892420c248bbf92899
Kristiana Blake <kristiana.blake@gmail.com>
2019-11-05 00:01:23 -0600

    delted review.js model

--- models/cafeList.js
+++ models/cafeList.js
@@ -2,33 +2,27 @@ const cafeList = [
 	{ 
 		name: "Goddess and the Baker",
 		address: "33 W Wabash Avenue"
-		review: String, 
 	},
 	{
 		name: "Dollop Coffee",
 		address: "801 S Financial Place"
-		review: String, 
 	},
 	{
 		name: "The Wormhole Coffee",
 		address: "1462 N Milwaukee Avenue"
-		review: String, 
 	},
 	{
 		name: "Swada Coffee",
 		address: "112 N Green Street"
-		review: String, 
 	},
 	{
 		name: "Cafe Integral at Freehand Chicago",
 		address: "19 E Ohio St"
-		review: String, 
 	},
 	{
 		name: "Stumptown Coffee Roasters",
-		address: "311 N Morgan St"
-		review: String, 
+		address: "311 N Morgan St" 
 	}
 ];
 
-module.exports = cafeList; 
+module.exports = cafeList
